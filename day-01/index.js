5144facca73fd49800f0e37663b2e58dc0546bd3
Hathaway-Shan <hathaway.shan@gmail.com>
2022-11-15 13:04:21 -0800

    test for oddish evenish problem is passing

--- day-01/index.js
+++ day-01/index.js
@@ -4,4 +4,21 @@ function reverseSentence(str) {
     .map((word) => word.split('').reverse().join(''))
     .join(' ');
 }
-module.exports = { reverseSentence };
+module.exports = { reverseSentence, oddishEvenish };
+
+function oddishEvenish(num) {
+  let digits = num.toString().split('');
+  const realDigits = digits.map(Number);
+
+  let sum = realDigits.reduce(
+    (previousValue, currentValue) => previousValue + currentValue
+  );
+
+  if (sum % 2 == 0) {
+    return 'Evenish';
+  } else {
+    return 'Oddish';
+  }
+}
+
+console.log(oddishEvenish(123));
--- day-01/index.test.js
+++ day-01/index.test.js
@@ -1,9 +1,14 @@
-const { reverseSentence } = require('./index');
+const { reverseSentence, oddishEvenish } = require('./index');
 
 describe('primitive code challenges', () => {
   describe('reverseSentence', () => {
     it('should reverse a sentence', () => {
       expect(reverseSentence('alchemy rocks gold')).toBe('ymehcla skcor dlog');
+    });
+  });
+  describe('oddishEvenish', () => {
+    it('sums the digits of a number and returns Oddish or Evenish', () => {
+      expect(oddishEvenish(123)).toBe('Evenish');
     });
   });
 });
